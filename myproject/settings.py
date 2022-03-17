69614b86bbbc4ec9d6511750d9d26d3ade24dee5
Duc-ju <trangduc56@gmail.com>
2022-03-17 23:04:59 +0700

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -216,6 +216,6 @@ ALLOWED_HOSTS = ['.HEROKUAPP.COM']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
